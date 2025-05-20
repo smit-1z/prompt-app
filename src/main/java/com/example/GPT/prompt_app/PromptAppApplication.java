a7de6050496946dbddbad6f38080c6349c9815ec
smit joshi <smit.joshi80@gmail.com>
2025-05-20 10:55:44 -0500

    Initial commit

---
+++ src/main/java/com/example/GPT/prompt_app/PromptAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.GPT.prompt_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptAppApplication.class, args);
+	}
+
+}
